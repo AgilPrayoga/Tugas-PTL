43fc440c781ead05ef02d749198bbc844ede0423
AgilPrayoga <117731684+AgilPrayoga@users.noreply.github.com>
2023-12-08 00:16:04 +0800

    update kesalahan algoritma

--- pages/metodewp.php
+++ pages/metodewp.php
@@ -4,120 +4,122 @@ include 'konsdb.php';
 
 ?>
 <div class="container pt-5">
-<h3 class="my-5 text-center ">Hasil Perhitungan Vector S</h3>
-<table class="table table-striped ">
-   <thead>
-      <tr class="table-primary">
-         <th>No</th>
-         <th>Alternatif</th>
-         <th>Vector S</th>
-      </tr>
-   </thead>
-   <?php
-   $nomor = 1;
-   $query = "SELECT * FROM alternatif";
-   $q_tampil_alternatif = mysqli_query($koneksi, $query) or die(mysqli_error($koneksi));
-   if (mysqli_num_rows($q_tampil_alternatif) > 0) {
-      $al = 0;
-      while ($r_tampil_alternatif = mysqli_fetch_array($q_tampil_alternatif)) {
-         $nmal[$al] = $r_tampil_alternatif['alternatif'];
-         $al++;
-      }
-   }
-
-
-   $sql = "SELECT * FROM kriteria";
-   $result = $koneksi->query($sql) or die(mysqli_error($koneksi));
-   if (mysqli_num_rows($result) > 0) {
-      $kr = 0;
-      $jmlbbt = 0;
-      while ($row = $result->fetch_assoc()) {
-         $krr[$kr] = $row['kriteria'];
-         $bbt[$kr] = $row['bobot'];
-         $atb[$kr] = $row['attribut'];
-         $jmlbbt = $jmlbbt + $bbt[$kr];
-         $kr++;
-      }
-      for ($y = 0; $y < $kr; $y++) {
-         $bbt[$y] = $bbt[$y] / $jmlbbt;
-      }
-   }
-
-
-   $sql = 'SELECT * FROM dataAL';
-   $result = $koneksi->query($sql) or die(mysqli_error($koneksi));
-   if (mysqli_num_rows($result) > 0) {
-      $x = 0;
-      while ($row = $result->fetch_assoc()) {
-         for ($i = 0; $i < $kr; $i++) {
-            $Valkrt[$x][$i] = $row[$krr[$i]];
-         }
-         $x++;
-      }
-   }
-   ?>
-   <tbody>
-      <?php
-      $jumlahVekS = 0;
-      for ($i = 0; $i < $al; $i++) {
-         $VekS[$i] = 1;
-         echo  "<tr>";
-         echo "<td>" . $nomor . "</td>";
-         echo "<td>" . $nmal[$i] . "</td>";
-
-         //hitung vektor 
-         for ($j = 0; $j < $kr; $j++) {
-            if ($atb[$j] == "benefit") {
-               $VekS[$i] = number_format($Valkrt[$i][$j] * number_format($bbt[$j], 2), 2);
-            } else {
-               $VekS[$i] = number_format($Valkrt[$i][$j] * -number_format($bbt[$j], 2), 2);
-            }
-            // echo  $Valkrt[$i][$j] . ',';
-         }
-         echo "<td>" . number_format($VekS[$i], 2) . "</td>";
-         echo "</tr>";
-         $nomor++;
-      }
-      $jumlahVekS = array_sum($VekS);
-      ?>
-   </tbody>
-</table>
-
-<h3 class ="text-center my-5">Hasil Perhitungan Nilai Vektor V</h3>
-<table class="table table-striped ">
-   <thead>
-      <tr class="table-success">
-         <th scope="col">No</th>
-         <th scope="col">Alternatif</th>
-         <th scope="col">Vector V</th>
-
-      </tr>
-   </thead>
-   <!-- php -->
-
-   <tbody>
+   <h3 class="my-5 text-center ">Hasil Perhitungan Vector S</h3>
+   <table class="table table-striped ">
+      <thead>
+         <tr class="table-primary">
+            <th>No</th>
+            <th>Alternatif</th>
+            <th>Vector S</th>
+         </tr>
+      </thead>
       <?php
       $nomor = 1;
-
-
-      for ($i = 0; $i < $al; $i++) {
-         $VekV[$i] = 1;
-         echo "<tr>";
-         echo "<td>" . $nomor . "</td>";
-         echo "<td>" . $nmal[$i] . "</td>";
-         $VekS[$i] = $VekS[$i] / $jumlahVekS;
-         // $jumlahVekS = $jumlahVekS + $VekS[$i];
-         echo "<td>" . number_format($VekS[$i], 2) . "</td>";
-         echo "</tr>";
-         $nomor++;
+      $query = "SELECT * FROM alternatif";
+      $q_tampil_alternatif = mysqli_query($koneksi, $query) or die(mysqli_error($koneksi));
+      if (mysqli_num_rows($q_tampil_alternatif) > 0) {
+         $al = 0;
+         while ($r_tampil_alternatif = mysqli_fetch_array($q_tampil_alternatif)) {
+            $nmal[$al] = $r_tampil_alternatif['alternatif'];
+            $al++;
+         }
       }
 
+      // mencari bobot
+      $sql = "SELECT * FROM kriteria";
+      $result = $koneksi->query($sql) or die(mysqli_error($koneksi));
+      if (mysqli_num_rows($result) > 0) {
+         $kr = 0;
+         $jmlbbt = 0;
+         while ($row = $result->fetch_assoc()) {
+            $krr[$kr] = $row['kriteria'];
+            $bbt[$kr] = $row['bobot'];
+            $atb[$kr] = $row['attribut'];
+            $jmlbbt = $jmlbbt + $bbt[$kr];
+            $kr++;
+         }
+         for ($y = 0; $y < $kr; $y++) {
+            $bbt[$y] = $bbt[$y] / $jmlbbt;
+         }
+      }
+      // mencari bobot
 
+
+      $sql = 'SELECT * FROM dataAL';
+      $result = $koneksi->query($sql) or die(mysqli_error($koneksi));
+      if (mysqli_num_rows($result) > 0) {
+         $x = 0;
+         while ($row = $result->fetch_assoc()) {
+            for ($i = 0; $i < $kr; $i++) {
+               $Valkrt[$x][$i] = $row[$krr[$i]];
+            }
+            $x++;
+         }
+      }
       ?>
-   </tbody>
+      <tbody>
+         <?php
+         $jumlahVekS = 0;
+         for ($i = 0; $i < $al; $i++) {
+            $VekS[$i] = 1;
+            echo  "<tr>";
+            echo "<td>" . $nomor . "</td>";
+            echo "<td>" . $nmal[$i] . "</td>";
 
-   <!-- php -->
-</table>
+            //hitung vektor 
+            for ($j = 0; $j < $kr; $j++) {
+               if ($atb[$j] == "benefit") {
+                  $VekS[$i] *= pow($Valkrt[$i][$j], number_format($bbt[$j], 5));
+               } else {
+                  $VekS[$i] *= pow($Valkrt[$i][$j], number_format(-$bbt[$j], 5));
+               }
+               // echo  $Valkrt[$i][$j] . ',';
+            }
+
+            echo "<td>" . number_format($VekS[$i], 4) . "</td>";
+            echo "</tr>";
+            $nomor++;
+         }
+         $jumlahVekS = array_sum($VekS);
+         ?>
+      </tbody>
+   </table>
+
+   <h3 class="text-center my-5">Hasil Perhitungan Nilai Vektor V</h3>
+   <table class="table table-striped ">
+      <thead>
+         <tr class="table-success">
+            <th scope="col">No</th>
+            <th scope="col">Alternatif</th>
+            <th scope="col">Vector V</th>
+
+         </tr>
+      </thead>
+      <!-- php -->
+
+      <tbody>
+         <?php
+         $nomor = 1;
+
+
+         for ($i = 0; $i < $al; $i++) {
+            $VekV[$i] = 1;
+            echo "<tr>";
+            echo "<td>" . $nomor . "</td>";
+            echo "<td>" . $nmal[$i] . "</td>";
+            $VekS[$i] = $VekS[$i] / $jumlahVekS;
+            // $jumlahVekS = $jumlahVekS + $VekS[$i];
+            echo "<td>" . number_format($VekS[$i], 4) . "</td>";
+            echo "</tr>";
+            $nomor++;
+         }
+
+
+         ?>
+      </tbody>
+
+      <!-- php -->
+   </table>
 </div>
 </div>
 <?php
@@ -127,7 +129,7 @@ include 'konsdb.php';
 <?php
 $datas = [];
 for ($i = 0; $i < $al; $i++) {
-   $data = ["nama" => $nmal[$i], "nilai" => number_format($VekS[$i], 3)];
+   $data = ["nama" => $nmal[$i], "nilai" => sprintf("%.4f", $VekS[$i])];
    $datas[] = $data;
 }
 
@@ -147,13 +149,15 @@ for ($xx = 0; $xx < $al; $xx++) {
 $datasUpdated = [];
 for ($i = 0; $i < $al; $i++) {
    for ($j = 0; $j < count($datas); $j++) {
-      if (number_format($VSort[$i], 3) == $datas[$j]["nilai"]) {
+      if (number_format($VSort[$i], 4) == $datas[$j]["nilai"]) {
          $datasUpdated[] = ["nama" => $datas[$j]["nama"], "nilai" => $datas[$j]["nilai"], "rank" => $i + 1];
       } else {
          continue;
       }
    }
 }
+
+
 
 
 $sql = "DROP TABLE rankwp";
@@ -177,7 +181,8 @@ if ($koneksi->query($sql) === TRUE) {
 for ($w = 0; $w < count($datasUpdated); $w++) {
    $nama = $datasUpdated[$w]["nama"];
    $rank = $datasUpdated[$w]["rank"];
-   $nilaiV = number_format($datasUpdated[$w]["nilai"], 2);
+   $nilaiV = number_format($datasUpdated[$w]["nilai"], 4);
+
    $sql = "INSERT INTO rankwp(`id`, `Nama`, `Rank`, `NilaiV`) VALUES ($w+1,'$nama',$rank,$nilaiV)";
    $query = mysqli_query($koneksi, $sql);
 }
